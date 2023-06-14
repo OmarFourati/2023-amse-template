b1e842aa42ebd1664aa4cda23f68a7ea3aeebf66
OmarFourati <fourati.omar@gmail.com>
2023-06-14 17:39:22 +0200

    test

--- tests.sh
+++ tests.sh
@@ -1,9 +1,17 @@
 echo ~~~~ Runing Pipeline ~~~~
 cd data
 python pipeline.py
+if [ $exit_status -ne 0 ]; then
+  echo "Error: Python script encountered an error."
+  exit $exit_status
+fi
 cd ..
 echo  ~~~~ Executing automated tests for the project. ~~~~ 
 cd project
 python tests.py
+if [ $exit_status -ne 0 ]; then
+  echo "Error: Python script encountered an error."
+  exit $exit_status
+fi
 cd ..
 echo ~~~~ Testing done. ~~~~
