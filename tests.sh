6b413b61a7aa66d42bd0d4d604e019616d035d0b
OmarFourati <fourati.omar@gmail.com>
2023-06-14 16:27:03 +0200

    update path of files to test

--- tests.sh
+++ tests.sh
@@ -1,5 +1,5 @@
 echo ~~~~ Runing Pipeline ~~~~
-python .data/pipeline.py
+python data/pipeline.py
 echo  ~~~~ Executing automated tests for the project. ~~~~ 
-python .project/tests.py
+python project/tests.py
 echo ~~~~ Testing done. ~~~~
