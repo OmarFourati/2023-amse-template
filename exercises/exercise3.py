a203e15fd7c085144a77cdc8d371393a6f3b9f74
OmarFourati <fourati.omar@gmail.com>
2023-06-19 09:38:22 +0200

    update data types

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -6,17 +6,9 @@ columns_for_sql = [0, 1, 2, 12, 22, 32, 42, 52, 62, 72]
 new_column_names = ["date", "CIN", "name", "petrol", "diesel", "gas", "electro", "hybrid", "plugInHybrid", "others"]
 df = pd.read_csv("https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv", sep=";", encoding="utf-8", encoding_errors="replace",skiprows=6, skipfooter=4, usecols=columns_for_sql)
 
-#Data Type for each column
-data_types = {
-    "date" : str,
-    "CIN" : str,
-    "name" : str,
-}     
-
 
 #new columns assignement
 df.set_axis(new_column_names, axis=1, inplace=True)
-
 df['petrol'] = pd.to_numeric(df['petrol'], errors='coerce', downcast='integer')
 df['diesel'] = pd.to_numeric(df['petrol'], errors='coerce', downcast='integer')
 df['gas'] = pd.to_numeric(df['gas'], errors='coerce', downcast='integer')
@@ -28,9 +20,10 @@ df['others'] = pd.to_numeric(df['others'], errors='coerce', downcast='integer')
 
 
 #Data Type assignement
-df = df.astype(data_types)
+print(df["name"])
 
 # CIN validation
+#validation for values > 0
 is_valid_cin = df['CIN'].apply(lambda x: bool(re.match(r'^0?\d{5}$', str(x))))
 df = df[df['electro'] > 0]
 df = df[df["hybrid"] > 0]
@@ -41,8 +34,22 @@ df = df[df['others'] > 0]
 df = df[df['diesel'] > 0]
 
 df = df.dropna()
-print(df.head())
-#validation for values > 0
+
+data_types = {
+    "date" : str,
+    "CIN" : str,
+    "name" : str,
+    "diesel": int,
+    "electro": int,
+    "gas": int,
+    "hybrid": int,
+    "plugInHybrid": int,
+    "others": int,
+    "petrol": int
+}    
+df = df.astype(data_types)
+
+
 
 
 df.to_sql("cars", "sqlite:///cars.sqlite", if_exists="replace", index=False)
